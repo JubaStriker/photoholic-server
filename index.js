246f8bc88617a28d20d12add6eab397111c6972c
JubaStriker <jubastriker@gmail.com>
2022-11-09 20:03:58 +0600

    data limited

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ycsbxjs.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 ;
 
@@ -23,6 +23,12 @@ async function run() {
         const serviceCollection = client.db('photoholic').collection('serviceCollection')
 
         app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+        app.get('/allServices', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
